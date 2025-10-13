7dfeaa36f71997345b6f1ab1dedbcb7b6afedc29
Gautam Kumar <gautamkumarxpvt@gmail.com>
2025-10-13 06:27:51 +0530

    start test

--- Healthcare_Management_System/settings.py
+++ Healthcare_Management_System/settings.py
@@ -81,8 +81,8 @@ else:
             'NAME': config('DATABASE_NAME'),
             'USER': config('DATABASE_USER'),
             'PASSWORD': config('DATABASE_PASSWORD'),
-            'HOST': config('DATABASE_HOST'),
-            'PORT': config('DATABASE_PORT', cast=int),
+            'HOST': config('DATABASE_HOST', default='localhost'),
+            'PORT': config('DATABASE_PORT', default='5432'),
         }
     }
 
